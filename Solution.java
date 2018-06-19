127e952bfab0b713bd6d0c442322e7288b9a1c14
divya1112 <40381164+divya1112@users.noreply.github.com>
2018-06-19 21:08:33 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Solution 
+{
+    public static void main(String args[] ) throws Exception 
+    {
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        int N = Integer.parseInt(a);
+        for (int i = 0; i < N; i++)
+        {
+            System.out.println("Hello");
+        }
+    }
+}
