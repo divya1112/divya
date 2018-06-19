c64f643eb38dd84e2a6fc58a0de17a6bce6bdb2d
divya1112 <40381164+divya1112@users.noreply.github.com>
2018-06-19 18:54:40 +0530

    Create Leapyr.java

---
+++ Leapyr.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class Leapyr
+{
+	public static void main(String[ ] arg)
+	{
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	if(a%4==0)
+	{
+	    System.out.print("yes");
+	}
+	else
+	{
+	    System.out.print("no");
+	}
+	}
+}
